094ac9e7adb28393fabf9e3f7445cfa406537fc6
unknown <sharifrayan952@gmail.com>
2025-02-25 22:41:50 -0800

    Western Leathers

--- frontend/src/components/Bestseller.jsx
+++ frontend/src/components/Bestseller.jsx
@@ -15,7 +15,7 @@ const Bestseller = () => {
     <>
       <div className="my-10">
         <div className="text-center py-8 text-3xl">
-          <Title text1={"PROVEN BEST"} text2={"SELLER"}></Title>
+          <Title text1={"BEST"} text2={"SELLER"}></Title>
           <p className="w-3/4 m-auto sm:text-sm md:text-base text-xs text-gray-600">
             "Fashion is very important It is life-enhancing and, like every
             thing that gives pleasure,it is worth doing well".
